71209ae2a825a4f39ecfc891bfb4823d8cba04ad
chen <2439146769@qq.com>
2019-03-20 14:46:46 +0800

    轮播图

---
+++ src/main/java/com/jk/bean/ImgInfo.java
@@ -0,0 +1,13 @@
+package com.jk.bean;
+
+import lombok.Data;
+
+@Data
+public class ImgInfo {
+
+    private Integer id;
+    private String imgName;
+    private String imgUrl;
+    private String tablename;
+    private Integer titleid;
+}
